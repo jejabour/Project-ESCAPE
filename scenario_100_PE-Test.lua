63991656adc6938b381d43e026a11c54c2a03793
jejabour <jejabour@gmail.com>
2023-09-13 16:27:38 -0500

    Added new scenario

---
+++ scenario_100_PE-Test.lua
@@ -0,0 +1,128 @@
+-- Name: PE Test Scenario
+-- Description: This is a test scenario for testing various aspects of the game
+-- Type: Mission
+
+
+-- ##########################################################################
+-- ## GM MENU ##
+-- ##########################################################################
+
+function gmMainMenu()
+    clearGMFunctions()
+    addGMFunction(_("buttonGM", "Scenario4      +", gmScenario4))
+    addGMFunction(_("buttonGM", "Alert Level         +"),gmAlertLevel)
+    addGMFunction(_("buttonGM", "Extra Commands      +"), gmUsefulCommmands)
+end
+
+function gmAlertLevel()
+    clearGMFunctions() -- Clear the menu
+    addGMFunction(_("buttonGM", "Alert level -"),gmMainMenu)
+    addGMFunction(_("buttonGM", "Normal"),gmAlertNormal)
+    addGMFunction(_("buttonGM", "Yellow"),gmAlertYellow)
+    addGMFunction(_("buttonGM", "Red"),gmAlertRed)
+end
+
+-- ##########################################################################
+-- ## GM Alert Level ##
+-- ##########################################################################
+
+function gmAlertNormal()
+    -- Clear and reset the menu
+    clearGMFunctions()
+    gmMainMenu()
+
+    alertLevel = "normal"
+end
+
+function gmAlertYellow()
+    -- Clear and reset the menu
+    clearGMFunctions()
+    gmMainMenu()
+
+    alertLevel = "yellow"
+end
+
+function gmAlertRed()
+    -- Clear and reset the menu
+    clearGMFunctions()
+    gmMainMenu()
+
+    alertLevel = "red"
+end
+
+-- ##########################################################################
+-- ## Extra Commands ##
+-- ##########################################################################
+function gmCreateCentralCommand()
+    gmMainMenu()
+    -- Home = setPosition(23500, 16100)
+
+    if central_command:getPosition() == nil then
+        central_command = SpaceStation():setTemplate("Small Station"):setFaction("Human Navy")
+        central_command:setPosition(23500, 16100):setCallSign("Central Command")
+
+    end
+end
+
+function gmClearMission()
+    -- Clear and reset the menu
+    clearGMFunctions()
+    gmMainMenu()
+
+    TraineeShip:destroy()
+
+    waveNumber = 0
+
+    alertLevel = "normal"
+
+    for _, friend in ipairs(friendList) do
+        if friend:isValid() then
+            friend:destroy()
+        end
+    end
+
+    for _, enemy in ipairs(enemyList) do
+        if enemy:isValid() then
+            enemy:destroy()
+        end
+    end
+
+end
+
+
+
+function init()
+    -- Setup GM menu
+    gmMainMenu()
+
+    -- Setup global variables
+    TraineeShip = {}
+    enemyList = {}
+    friendList = {}
+    waveNumber = 0
+    alertLevel = "normal"
+
+    -- Create the command station
+    central_command = SpaceStation():setTemplate("Small Station"):setFaction("Human Navy")
+    central_command:setPosition(23500, 16100):setCallSign("Central Command")
+
+    -- Nebula that hide the enemy station.
+    Nebula():setPosition(-43300, 2200)
+    Nebula():setPosition(-34000, -700)
+    Nebula():setPosition(-32000, -10000)
+    Nebula():setPosition(-24000, -14300)
+    Nebula():setPosition(-28600, -21900)
+
+    -- Random nebulae in the system
+    Nebula():setPosition(-8000, -38300)
+    Nebula():setPosition(24000, -30700)
+    Nebula():setPosition(42300, 3100)
+    Nebula():setPosition(49200, 10700)
+    Nebula():setPosition(3750, 31250)
+    Nebula():setPosition(-39500, 18700)
+
+    -- Create 50 Asteroids
+    placeRandom(Asteroid, 50, -7500, -10000, -12500, 30000, 2000)
+    placeRandom(VisualAsteroid, 50, -7500, -10000, -12500, 30000, 2000)
+
+end
